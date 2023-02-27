3129b32e4bdc4099ddc608cb83d11851d9e4f08b
Sourav2707 <smartsourav33@gmail.com>
2023-02-27 15:27:11 +0530

    Create a simple html file to test the REST services with JSON Server using Promise

--- js/ajaxCallback.js
+++ js/ajaxCallback.js
@@ -23,7 +23,8 @@ function makeAJAXCall(methodType, url, callback, async = true, data = null){
 function promiseCall(methodType, url, callback, async = true, data = null){
     return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
+    // xhr.onreadystatechange = function()
+    xhr.onload = function(){
         console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
         if (xhr.readyState == 4) {
             resolve(xhr.responseText);            
@@ -35,19 +36,32 @@ function promiseCall(methodType, url, callback, async = true, data = null){
             console.log("Handle 400 client Error or 500 server Error");
         }
     }
+    xhr.onerror = function() {
+        reject({
+            status : this.status,
+            statusText : xhttp.statusText
+        });
+    };
     xhr.open(methodType, url, async);
-    xhr.send();
+    if(data) {
+        console.log(JSON.stringify(data));
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }
+    else {
+        xhr.send();
+    }
     console.log(methodType+" request send to server at "+showTime());
 });
 }
-const getURL = "http://localhost:3000/employees/1";
+// const getURL = "http://localhost:3000/employees/1";
 // function getUserDetails(data){
 //     console.log("Get User Data at : " + showTime() + " data: " + data)
 // }
 // makeAJAXCall("GET", getURL, getUserDetails, true);
-//promise
-promiseCall("GET", getURL, true)
-    .then(responseText => {
-    console.log("Get User Data: "+responseText)
-})
-.catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
+
+// promiseCall("GET", getURL, true)
+//     .then(responseText => {
+//     console.log("Get User Data: "+responseText)
+// })
+// .catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
