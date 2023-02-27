f1e0830298e8a4df66ce07cbcd07f865fbe02b03
Sourav2707 <smartsourav33@gmail.com>
2023-02-27 14:56:33 +0530

    Ability to view Employee Data from JSON Server having ID, Name and Salary using AJAX and Promise

--- js/ajaxCallback.js
+++ js/ajaxCallback.js
@@ -4,6 +4,7 @@ function showTime(){
     return date.getHours + " Hrs, " + date.getMinutes + " Min " + date.getSeconds() + "Secs";
 }
 
+//callback
 function makeAJAXCall(methodType, url, callback, async = true, data = null){
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
@@ -18,9 +19,35 @@ function makeAJAXCall(methodType, url, callback, async = true, data = null){
     xhr.send();
     console.log(methodType+" request send to server at "+showTime());
 }
-
-const getURL = "http://localhost:3000/employees/1";
-function getUserDetails(data){
-    console.log("Get User Data at : " + showTime() + " data: " + data)
+//promise
+function promiseCall(methodType, url, callback, async = true, data = null){
+    return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
+        if (xhr.readyState == 4) {
+            resolve(xhr.responseText);            
+        } else if (xhr.status >= 400){
+            reject({
+                status : xhr.status,
+                statusText : xhr.statusText
+            });
+            console.log("Handle 400 client Error or 500 server Error");
+        }
+    }
+    xhr.open(methodType, url, async);
+    xhr.send();
+    console.log(methodType+" request send to server at "+showTime());
+});
 }
-makeAJAXCall("GET", getURL, getUserDetails, true);
+const getURL = "http://localhost:3000/employees/1";
+// function getUserDetails(data){
+//     console.log("Get User Data at : " + showTime() + " data: " + data)
+// }
+// makeAJAXCall("GET", getURL, getUserDetails, true);
+//promise
+promiseCall("GET", getURL, true)
+    .then(responseText => {
+    console.log("Get User Data: "+responseText)
+})
+.catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
